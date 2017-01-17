e7fe539c737194916bde890952ea5f806a81af8e
Florent Kermarrec <florent@enjoy-digital.fr>
2017-01-17 13:48:03 +0100

    frontend/bist: remove LiteDRAMBISTCheckerScope.
    Checker should not be used to investigate errors but only to verify that is already validated still works. (ie we don't want to be able to understand what is going on, just to know if it's working or not). To understand what is going on we will look at signals with LiteScope and eventually trigger on err_count from checker.

--- litedram/frontend/bist.py
+++ litedram/frontend/bist.py
@@ -322,30 +322,3 @@ class LiteDRAMBISTChecker(Module, AutoCSR):
             err_count_sync.i.eq(core.err_count),
             self.err_count.status.eq(err_count_sync.o),
         ]
-
-
-class LiteDRAMBISTCheckerScope(Module):
-    """Easy scope access to important signals of LiteDRAMBISTChecker."""
-
-    def __init__(self, checker):
-        core = checker.core
-        self.data_error = Signal()
-        self.data_address = Signal(core.data_counter.nbits)
-        self.data_expected = Signal(core.dma.source.data.nbits)
-        self.data_actual = Signal(core.dma.source.data.nbits)
-
-        self.comb += [
-            self.data_error.eq(core.dma.source.valid &
-                               (self.data_actual != self.data_expected)),
-            self.data_address.eq(core.base + core.data_counter),
-            self.data_actual.eq(core.dma.source.data),
-            self.data_expected.eq(core.gen.o),
-        ]
-
-    def signals(self):
-        return [
-            self.data_error,
-            self.data_address,
-            self.data_expected,
-            self.data_actual,
-        ]
