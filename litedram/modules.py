2bd7707e678b12d9c980e9480ee98ec6cd7dc11d
Florent Kermarrec <florent@enjoy-digital.fr>
2018-06-12 09:54:38 +0200

    modules: add MT18KSF1G72HZ_1G6

--- litedram/modules.py
+++ litedram/modules.py
@@ -247,3 +247,18 @@ class MT41J256M16(SDRAMModule):
     tWTR  = 8
     tREFI = 64*1000*1000/8192
     tRFC  = 260
+
+
+class MT18KSF1G72HZ_1G6(SDRAMModule):
+    memtype = "DDR3"
+    # geometry
+    nbanks = 8
+    nrows  = 65536
+    ncols  = 1024
+    # timings (-125 speedgrade)
+    tRP   = 13.75
+    tRCD  = 13.75
+    tWR   = 15
+    tWTR  = 8
+    tREFI = 64*1000*1000/8192
+    tRFC  = 260
