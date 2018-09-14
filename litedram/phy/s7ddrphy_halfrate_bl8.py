a8d26724ddf8f75ee29a9a2986080dba037b05b0
Florent Kermarrec <florent@enjoy-digital.fr>
2018-09-15 01:50:53 +0200

    phy/s7ddrphy_halfrate_bl8: don't generate dqs pre/post-amble, needs simulation

--- litedram/phy/s7ddrphy_halfrate_bl8.py
+++ litedram/phy/s7ddrphy_halfrate_bl8.py
@@ -444,10 +444,10 @@ class S7DDRPHY(Module, AutoCSR):
         elif memtype == "DDR3":
             dqs_sys_latency = cwl_sys_latency-1 if with_odelay else cwl_sys_latency
         self.comb += [
-            dqs_preamble.eq(last_wrdata_en[dqs_sys_latency-1] &
-                            ~last_wrdata_en[dqs_sys_latency]),
-            dqs_postamble.eq(last_wrdata_en[dqs_sys_latency+2] &
-                            ~last_wrdata_en[dqs_sys_latency+1]),
+            #dqs_preamble.eq(last_wrdata_en[dqs_sys_latency-1] &
+            #                ~last_wrdata_en[dqs_sys_latency]),
+            #dqs_postamble.eq(last_wrdata_en[dqs_sys_latency+2] &
+            #                ~last_wrdata_en[dqs_sys_latency+1]),
         ]
 
 
