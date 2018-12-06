1618a7636ab788084619b4528a11c8c845bc4d48
Florent Kermarrec <florent@enjoy-digital.fr>
2018-12-06 21:15:47 +0100

    phy: add KUSDDRPHY to __init__.py

--- litedram/phy/__init__.py
+++ litedram/phy/__init__.py
@@ -1,6 +1,7 @@
 from litedram.phy.gensdrphy import GENSDRPHY
 from litedram.phy.s6ddrphy import S6HalfRateDDRPHY, S6QuarterRateDDRPHY
 from litedram.phy.s7ddrphy import V7DDRPHY, K7DDRPHY, A7DDRPHY
+from litedram.phy.kusddrphy import KUSDDRPHY
 
 # backward compatibility (remove when no longer needed)
 from litedram.phy import s7ddrphy as a7ddrphy
