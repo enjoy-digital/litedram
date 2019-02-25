9057f510d24f22756e1a8ad3f875941eb14b1e16
Florent Kermarrec <florent@enjoy-digital.fr>
2019-02-25 15:30:04 +0100

    phy: add ECP5 imports

--- litedram/phy/__init__.py
+++ litedram/phy/__init__.py
@@ -1,7 +1,10 @@
 from litedram.phy.gensdrphy import GENSDRPHY
+
 from litedram.phy.s6ddrphy import S6HalfRateDDRPHY, S6QuarterRateDDRPHY
 from litedram.phy.s7ddrphy import V7DDRPHY, K7DDRPHY, A7DDRPHY
 from litedram.phy.usddrphy import USDDRPHY
+
+from litedram.phy.ecp5ddrphy import ECP5DDRPHY, ECP5DDRPHYInit
 
 # backward compatibility (remove when no longer needed)
 from litedram.phy import s7ddrphy as a7ddrphy
