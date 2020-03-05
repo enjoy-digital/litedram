34d17949fdf805adf8caee93fbff4e11a7d2b14f
Mariusz Glebocki <mglebocki@antmicro.com>
2020-03-05 10:19:04 +0100

    test: update reference files (*_init.h)

--- test/reference/ddr4_init.h
+++ test/reference/ddr4_init.h
@@ -1,61 +1,43 @@
 #ifndef __GENERATED_SDRAM_PHY_H
 #define __GENERATED_SDRAM_PHY_H
+
 #include <hw/common.h>
 #include <generated/csr.h>
 #include <hw/flags.h>
+#include <stdint.h>
 
-#define DFII_NPHASES 4
+#define DFII_NPHASES_MAX       4
+#define DFII_PIX_DATA_SIZE_MAX 16
 
 static void cdelay(int i);
 
-__attribute__((unused)) static void command_p0(int cmd)
+
+static void sdram_phy_sdram_command_p0(uint8_t cmd)
 {
-    sdram_dfii_pi0_command_write(cmd);
-    sdram_dfii_pi0_command_issue_write(1);
-}
-__attribute__((unused)) static void command_p1(int cmd)
-{
-    sdram_dfii_pi1_command_write(cmd);
-    sdram_dfii_pi1_command_issue_write(1);
-}
-__attribute__((unused)) static void command_p2(int cmd)
-{
-    sdram_dfii_pi2_command_write(cmd);
-    sdram_dfii_pi2_command_issue_write(1);
-}
-__attribute__((unused)) static void command_p3(int cmd)
-{
-    sdram_dfii_pi3_command_write(cmd);
-    sdram_dfii_pi3_command_issue_write(1);
+	sdram_dfii_pi0_command_write(cmd);
+	sdram_dfii_pi0_command_issue_write(cmd);
 }
 
+static void sdram_phy_sdram_command_p1(uint8_t cmd)
+{
+	sdram_dfii_pi1_command_write(cmd);
+	sdram_dfii_pi1_command_issue_write(cmd);
+}
 
-#define sdram_dfii_pird_address_write(X) sdram_dfii_pi1_address_write(X)
-#define sdram_dfii_piwr_address_write(X) sdram_dfii_pi3_address_write(X)
-#define sdram_dfii_pird_baddress_write(X) sdram_dfii_pi1_baddress_write(X)
-#define sdram_dfii_piwr_baddress_write(X) sdram_dfii_pi3_baddress_write(X)
-#define command_prd(X) command_p1(X)
-#define command_pwr(X) command_p3(X)
+static void sdram_phy_sdram_command_p2(uint8_t cmd)
+{
+	sdram_dfii_pi2_command_write(cmd);
+	sdram_dfii_pi2_command_issue_write(cmd);
+}
 
-#define DFII_PIX_DATA_SIZE CSR_SDRAM_DFII_PI0_WRDATA_SIZE
+static void sdram_phy_sdram_command_p3(uint8_t cmd)
+{
+	sdram_dfii_pi3_command_write(cmd);
+	sdram_dfii_pi3_command_issue_write(cmd);
+}
 
-const unsigned long sdram_dfii_pix_wrdata_addr[DFII_NPHASES] = {
-	CSR_SDRAM_DFII_PI0_WRDATA_ADDR,
-	CSR_SDRAM_DFII_PI1_WRDATA_ADDR,
-	CSR_SDRAM_DFII_PI2_WRDATA_ADDR,
-	CSR_SDRAM_DFII_PI3_WRDATA_ADDR
-};
 
-const unsigned long sdram_dfii_pix_rddata_addr[DFII_NPHASES] = {
-	CSR_SDRAM_DFII_PI0_RDDATA_ADDR,
-	CSR_SDRAM_DFII_PI1_RDDATA_ADDR,
-	CSR_SDRAM_DFII_PI2_RDDATA_ADDR,
-	CSR_SDRAM_DFII_PI3_RDDATA_ADDR
-};
-
-#define DDRX_MR1 769
-
-static void init_sequence(void)
+static void sdram_phy_sdram_init_sequence(void)
 {
 	/* Release reset */
 	sdram_dfii_pi0_address_write(0x0);
@@ -72,44 +54,177 @@ static void init_sequence(void)
 	/* Load Mode Register 3 */
 	sdram_dfii_pi0_address_write(0x0);
 	sdram_dfii_pi0_baddress_write(3);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
 
 	/* Load Mode Register 6 */
 	sdram_dfii_pi0_address_write(0x0);
 	sdram_dfii_pi0_baddress_write(6);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
 
 	/* Load Mode Register 5 */
 	sdram_dfii_pi0_address_write(0x0);
 	sdram_dfii_pi0_baddress_write(5);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
 
 	/* Load Mode Register 4 */
 	sdram_dfii_pi0_address_write(0x0);
 	sdram_dfii_pi0_baddress_write(4);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
 
 	/* Load Mode Register 2, CWL=9 */
 	sdram_dfii_pi0_address_write(0x200);
 	sdram_dfii_pi0_baddress_write(2);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
 
 	/* Load Mode Register 1 */
 	sdram_dfii_pi0_address_write(0x301);
 	sdram_dfii_pi0_baddress_write(1);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
 
 	/* Load Mode Register 0, CL=11, BL=8 */
 	sdram_dfii_pi0_address_write(0x110);
 	sdram_dfii_pi0_baddress_write(0);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
 	cdelay(200);
 
 	/* ZQ Calibration */
 	sdram_dfii_pi0_address_write(0x400);
 	sdram_dfii_pi0_baddress_write(0);
-	command_p0(DFII_COMMAND_WE|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_WE|DFII_COMMAND_CS);
 	cdelay(200);
-
 }
-#endif
+
+
+static inline void sdram_phy_init_all(void)
+{
+	sdram_phy_sdram_init_sequence();
+}
+
+
+struct sdram_phy_t {
+	uint8_t nphases;
+	uint8_t pix_data_size;
+	uint16_t ddrx_mr1;
+
+	unsigned long pix_wrdata_addr[DFII_NPHASES_MAX];
+	unsigned long pix_rddata_addr[DFII_NPHASES_MAX];
+
+	void (* control_write)(uint8_t v);
+
+	void (* pix_command_write[DFII_NPHASES_MAX])(uint8_t v);
+	void (* pix_command_issue_write[DFII_NPHASES_MAX])(uint8_t v);
+
+	void (* pix_address_write[DFII_NPHASES_MAX])(uint16_t v);
+	void (* pird_address_write)(uint16_t v);
+	void (* piwr_address_write)(uint16_t v);
+
+	void (* pix_baddress_write[DFII_NPHASES_MAX])(uint8_t v);
+	void (* pird_baddress_write)(uint8_t v);
+	void (* piwr_baddress_write)(uint8_t v);
+
+	void (* command_px[DFII_NPHASES_MAX])(uint8_t cmd);
+	void (* command_prd)(uint8_t cmd);
+	void (* command_pwr)(uint8_t cmd);
+
+	void (* init)(void);
+};
+
+static const struct sdram_phy_t sdram_phys[] = {
+	/* sdram */
+	{
+		4,
+		CSR_SDRAM_DFII_PI0_WRDATA_SIZE,
+		0x301,
+		{
+			CSR_SDRAM_DFII_PI0_WRDATA_ADDR,
+			CSR_SDRAM_DFII_PI1_WRDATA_ADDR,
+			CSR_SDRAM_DFII_PI2_WRDATA_ADDR,
+			CSR_SDRAM_DFII_PI3_WRDATA_ADDR
+		},
+		{
+			CSR_SDRAM_DFII_PI0_RDDATA_ADDR,
+			CSR_SDRAM_DFII_PI1_RDDATA_ADDR,
+			CSR_SDRAM_DFII_PI2_RDDATA_ADDR,
+			CSR_SDRAM_DFII_PI3_RDDATA_ADDR
+		},
+		sdram_dfii_control_write,
+		{
+			sdram_dfii_pi0_command_write,
+			sdram_dfii_pi1_command_write,
+			sdram_dfii_pi2_command_write,
+			sdram_dfii_pi3_command_write
+		},
+		{
+			sdram_dfii_pi0_command_issue_write,
+			sdram_dfii_pi1_command_issue_write,
+			sdram_dfii_pi2_command_issue_write,
+			sdram_dfii_pi3_command_issue_write
+		},
+		{
+			sdram_dfii_pi0_address_write,
+			sdram_dfii_pi1_address_write,
+			sdram_dfii_pi2_address_write,
+			sdram_dfii_pi3_address_write
+		},
+		sdram_dfii_pi1_address_write, /* rd */
+		sdram_dfii_pi3_address_write, /* wr */
+		{
+			sdram_dfii_pi0_baddress_write,
+			sdram_dfii_pi1_baddress_write,
+			sdram_dfii_pi2_baddress_write,
+			sdram_dfii_pi3_baddress_write
+		},
+		sdram_dfii_pi1_baddress_write, /* rd */
+		sdram_dfii_pi3_baddress_write, /* wr */
+		{
+			sdram_phy_sdram_command_p0,
+			sdram_phy_sdram_command_p1,
+			sdram_phy_sdram_command_p2,
+			sdram_phy_sdram_command_p3
+		},
+		sdram_phy_sdram_command_p1, /* rd */
+		sdram_phy_sdram_command_p3, /* wr */
+		sdram_phy_sdram_init_sequence
+	}
+};
+
+
+/*** backward compatibility ***/
+
+#ifndef SDRAM_PHY_DISABLE_BACKWARD_COMPATIBILITY
+
+#define DFII_NPHASES 4
+
+static inline __attribute__((always_inline)) void command_p0(uint8_t v) { sdram_phy_sdram_command_p0(v); }
+static inline __attribute__((always_inline)) void command_p1(uint8_t v) { sdram_phy_sdram_command_p1(v); }
+static inline __attribute__((always_inline)) void command_p2(uint8_t v) { sdram_phy_sdram_command_p2(v); }
+static inline __attribute__((always_inline)) void command_p3(uint8_t v) { sdram_phy_sdram_command_p3(v); }
+static inline __attribute__((always_inline)) void sdram_dfii_pird_address_write(uint16_t v) { sdram_dfii_pi1_address_write(v); }
+static inline __attribute__((always_inline)) void sdram_dfii_piwr_address_write(uint16_t v) { sdram_dfii_pi3_address_write(v); }
+static inline __attribute__((always_inline)) void sdram_dfii_pird_baddress_write(uint8_t v) { sdram_dfii_pi1_baddress_write(v); }
+static inline __attribute__((always_inline)) void sdram_dfii_piwr_baddress_write(uint8_t v) { sdram_dfii_pi3_baddress_write(v); }
+static inline __attribute__((always_inline)) void command_prd(uint8_t v) { sdram_phy_sdram_command_p1(v); }
+static inline __attribute__((always_inline)) void command_pwr(uint8_t v) { sdram_phy_sdram_command_p3(v); }
+
+#define DFII_PIX_DATA_SIZE CSR_SDRAM_DFII_PI0_WRDATA_SIZE
+
+const unsigned long sdram_dfii_pix_wrdata_addr[DFII_NPHASES] = {
+	CSR_SDRAM_DFII_PI0_WRDATA_ADDR,
+	CSR_SDRAM_DFII_PI1_WRDATA_ADDR,
+	CSR_SDRAM_DFII_PI2_WRDATA_ADDR,
+	CSR_SDRAM_DFII_PI3_WRDATA_ADDR
+};
+const unsigned long sdram_dfii_pix_rddata_addr[DFII_NPHASES] = {
+	CSR_SDRAM_DFII_PI0_RDDATA_ADDR,
+	CSR_SDRAM_DFII_PI1_RDDATA_ADDR,
+	CSR_SDRAM_DFII_PI2_RDDATA_ADDR,
+	CSR_SDRAM_DFII_PI3_RDDATA_ADDR
+};
+
+#define DDRX_MR1 769
+
+static inline __attribute__((always_inline)) void init_sequence(void) { sdram_phy_init_all(); }
+
+#endif /* SDRAM_PHY_DISABLE_BACKWARD_COMPATIBILITY */
+
+#endif /* __GENERATED_SDRAM_PHY_H */
--- test/reference/sdr_init.h
+++ test/reference/sdr_init.h
@@ -1,38 +1,25 @@
 #ifndef __GENERATED_SDRAM_PHY_H
 #define __GENERATED_SDRAM_PHY_H
+
 #include <hw/common.h>
 #include <generated/csr.h>
 #include <hw/flags.h>
+#include <stdint.h>
 
-#define DFII_NPHASES 1
+#define DFII_NPHASES_MAX       1
+#define DFII_PIX_DATA_SIZE_MAX 2
 
 static void cdelay(int i);
 
-__attribute__((unused)) static void command_p0(int cmd)
+
+static void sdram_phy_sdram_command_p0(uint8_t cmd)
 {
-    sdram_dfii_pi0_command_write(cmd);
-    sdram_dfii_pi0_command_issue_write(1);
+	sdram_dfii_pi0_command_write(cmd);
+	sdram_dfii_pi0_command_issue_write(cmd);
 }
 
 
-#define sdram_dfii_pird_address_write(X) sdram_dfii_pi0_address_write(X)
-#define sdram_dfii_piwr_address_write(X) sdram_dfii_pi0_address_write(X)
-#define sdram_dfii_pird_baddress_write(X) sdram_dfii_pi0_baddress_write(X)
-#define sdram_dfii_piwr_baddress_write(X) sdram_dfii_pi0_baddress_write(X)
-#define command_prd(X) command_p0(X)
-#define command_pwr(X) command_p0(X)
-
-#define DFII_PIX_DATA_SIZE CSR_SDRAM_DFII_PI0_WRDATA_SIZE
-
-const unsigned long sdram_dfii_pix_wrdata_addr[DFII_NPHASES] = {
-	CSR_SDRAM_DFII_PI0_WRDATA_ADDR
-};
-
-const unsigned long sdram_dfii_pix_rddata_addr[DFII_NPHASES] = {
-	CSR_SDRAM_DFII_PI0_RDDATA_ADDR
-};
-
-static void init_sequence(void)
+static void sdram_phy_sdram_init_sequence(void)
 {
 	/* Bring CKE high */
 	sdram_dfii_pi0_address_write(0x0);
@@ -43,36 +30,137 @@ static void init_sequence(void)
 	/* Precharge All */
 	sdram_dfii_pi0_address_write(0x400);
 	sdram_dfii_pi0_baddress_write(0);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
 
 	/* Load Mode Register / Reset DLL, CL=2, BL=1 */
 	sdram_dfii_pi0_address_write(0x120);
 	sdram_dfii_pi0_baddress_write(0);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
 	cdelay(200);
 
 	/* Precharge All */
 	sdram_dfii_pi0_address_write(0x400);
 	sdram_dfii_pi0_baddress_write(0);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
 
 	/* Auto Refresh */
 	sdram_dfii_pi0_address_write(0x0);
 	sdram_dfii_pi0_baddress_write(0);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_CS);
 	cdelay(4);
 
 	/* Auto Refresh */
 	sdram_dfii_pi0_address_write(0x0);
 	sdram_dfii_pi0_baddress_write(0);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_CS);
 	cdelay(4);
 
 	/* Load Mode Register / CL=2, BL=1 */
 	sdram_dfii_pi0_address_write(0x20);
 	sdram_dfii_pi0_baddress_write(0);
-	command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
+	sdram_phy_sdram_command_p0(DFII_COMMAND_RAS|DFII_COMMAND_CAS|DFII_COMMAND_WE|DFII_COMMAND_CS);
 	cdelay(200);
-
 }
-#endif
+
+
+static inline void sdram_phy_init_all(void)
+{
+	sdram_phy_sdram_init_sequence();
+}
+
+
+struct sdram_phy_t {
+	uint8_t nphases;
+	uint8_t pix_data_size;
+	uint16_t ddrx_mr1;
+
+	unsigned long pix_wrdata_addr[DFII_NPHASES_MAX];
+	unsigned long pix_rddata_addr[DFII_NPHASES_MAX];
+
+	void (* control_write)(uint8_t v);
+
+	void (* pix_command_write[DFII_NPHASES_MAX])(uint8_t v);
+	void (* pix_command_issue_write[DFII_NPHASES_MAX])(uint8_t v);
+
+	void (* pix_address_write[DFII_NPHASES_MAX])(uint16_t v);
+	void (* pird_address_write)(uint16_t v);
+	void (* piwr_address_write)(uint16_t v);
+
+	void (* pix_baddress_write[DFII_NPHASES_MAX])(uint8_t v);
+	void (* pird_baddress_write)(uint8_t v);
+	void (* piwr_baddress_write)(uint8_t v);
+
+	void (* command_px[DFII_NPHASES_MAX])(uint8_t cmd);
+	void (* command_prd)(uint8_t cmd);
+	void (* command_pwr)(uint8_t cmd);
+
+	void (* init)(void);
+};
+
+static const struct sdram_phy_t sdram_phys[] = {
+	/* sdram */
+	{
+		1,
+		CSR_SDRAM_DFII_PI0_WRDATA_SIZE,
+		0x0,
+		{
+			CSR_SDRAM_DFII_PI0_WRDATA_ADDR
+		},
+		{
+			CSR_SDRAM_DFII_PI0_RDDATA_ADDR
+		},
+		sdram_dfii_control_write,
+		{
+			sdram_dfii_pi0_command_write
+		},
+		{
+			sdram_dfii_pi0_command_issue_write
+		},
+		{
+			sdram_dfii_pi0_address_write
+		},
+		sdram_dfii_pi0_address_write, /* rd */
+		sdram_dfii_pi0_address_write, /* wr */
+		{
+			sdram_dfii_pi0_baddress_write
+		},
+		sdram_dfii_pi0_baddress_write, /* rd */
+		sdram_dfii_pi0_baddress_write, /* wr */
+		{
+			sdram_phy_sdram_command_p0
+		},
+		sdram_phy_sdram_command_p0, /* rd */
+		sdram_phy_sdram_command_p0, /* wr */
+		sdram_phy_sdram_init_sequence
+	}
+};
+
+
+/*** backward compatibility ***/
+
+#ifndef SDRAM_PHY_DISABLE_BACKWARD_COMPATIBILITY
+
+#define DFII_NPHASES 1
+
+static inline __attribute__((always_inline)) void command_p0(uint8_t v) { sdram_phy_sdram_command_p0(v); }
+static inline __attribute__((always_inline)) void sdram_dfii_pird_address_write(uint16_t v) { sdram_dfii_pi0_address_write(v); }
+static inline __attribute__((always_inline)) void sdram_dfii_piwr_address_write(uint16_t v) { sdram_dfii_pi0_address_write(v); }
+static inline __attribute__((always_inline)) void sdram_dfii_pird_baddress_write(uint8_t v) { sdram_dfii_pi0_baddress_write(v); }
+static inline __attribute__((always_inline)) void sdram_dfii_piwr_baddress_write(uint8_t v) { sdram_dfii_pi0_baddress_write(v); }
+static inline __attribute__((always_inline)) void command_prd(uint8_t v) { sdram_phy_sdram_command_p0(v); }
+static inline __attribute__((always_inline)) void command_pwr(uint8_t v) { sdram_phy_sdram_command_p0(v); }
+
+#define DFII_PIX_DATA_SIZE CSR_SDRAM_DFII_PI0_WRDATA_SIZE
+
+const unsigned long sdram_dfii_pix_wrdata_addr[DFII_NPHASES] = {
+	CSR_SDRAM_DFII_PI0_WRDATA_ADDR
+};
+const unsigned long sdram_dfii_pix_rddata_addr[DFII_NPHASES] = {
+	CSR_SDRAM_DFII_PI0_RDDATA_ADDR
+};
+
+static inline __attribute__((always_inline)) void init_sequence(void) { sdram_phy_init_all(); }
+
+#endif /* SDRAM_PHY_DISABLE_BACKWARD_COMPATIBILITY */
+
+#endif /* __GENERATED_SDRAM_PHY_H */
