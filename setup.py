0c3a610544409bc0a15c11ac806b151680aa21ca
Florent Kermarrec <florent@enjoy-digital.fr>
2020-04-07 11:51:23 +0200

    setup.py: simplify, switch to Python3.6+ (using python_requires), remove version.
    - Deprecate Python 3.5, switch to Python 3.6+.
    - Remove which was not used or updated. We'll see to get this back when working on releases.

--- setup.py
+++ setup.py
@@ -1,37 +1,20 @@
 #!/usr/bin/env python3
 
-import sys
 from setuptools import setup
 from setuptools import find_packages
 
 
-if sys.version_info[:3] < (3, 5):
-    raise SystemExit("You need Python 3.5+")
-
-
 setup(
     name="litedram",
-    version="0.2.dev",
     description="Small footprint and configurable DRAM core",
-    long_description=open("README").read(),
     author="Florent Kermarrec",
     author_email="florent@enjoy-digital.fr",
     url="http://enjoy-digital.fr",
     download_url="https://github.com/enjoy-digital/litedram",
     test_suite="test",
-    install_requires=['pyyaml'],
     license="BSD",
-    platforms=["Any"],
-    keywords="HDL ASIC FPGA hardware design",
-    classifiers=[
-        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
-        "Environment :: Console",
-        "Development Status :: Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-    ],
+    python_requires="~=3.6",
+    install_requires=["pyyaml"],
     packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
     include_package_data=True,
     entry_points={
