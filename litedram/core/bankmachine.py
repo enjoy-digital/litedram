51e3143c1d0fecdb13e28e831fb0dd11d31ba906
pbrazzle <prb50@pitt.edu>
2022-07-22 10:40:02 -0400

    TMRBufferReq

--- litedram/core/bankmachine.py
+++ litedram/core/bankmachine.py
@@ -120,8 +120,8 @@ class BankMachine(Module):
         cmd_buffer = stream.Buffer(cmd_buffer_layout) # 1 depth buffer to detect row change
         self.submodules += cmd_buffer_lookahead, cmd_buffer
         self.comb += [
-            req.connect(cmd_buffer_lookahead.sink, keep={"valid", "ready", "we", "addr"}),
-            TMRreq.ready.eq(cmd_buffer_lookahead.sink.ready),
+            TMRBufferReq.connect(cmd_buffer_lookahead.sink, keep={"valid", "ready", "we", "addr"}),
+            #TMRreq.ready.eq(cmd_buffer_lookahead.sink.ready),
             #cmd_buffer_lookahead.sink.valid.eq(TMRreq.valid),
             #cmd_buffer_lookahead.sink.we.eq(TMRreq.we),
             #cmd_buffer_lookahead.sink.addr.eq(TMRreq.addr),
