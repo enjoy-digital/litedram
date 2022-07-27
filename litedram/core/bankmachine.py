6a753ed10ae566209a0807b198e139f8c2a30484
pbrazzle <prb50@pitt.edu>
2022-07-27 13:51:41 -0400

    bank machine tmr

--- litedram/core/bankmachine.py
+++ litedram/core/bankmachine.py
@@ -88,7 +88,6 @@ class BankMachine(Module):
     """
     def __init__(self, n, address_width, address_align, nranks, settings):
         self.req = req = Record(cmd_layout(address_width))
-        self.req_copy = req_copy = Record(cmd_layout(address_width))
         self.TMRreq = TMRreq = TMRRecord(req)
         self.refresh_req = refresh_req = Signal()
         self.refresh_gnt = refresh_gnt = Signal()
@@ -113,14 +112,10 @@ class BankMachine(Module):
         
         weTMRIn = TMRInput(TMRreq.we)
         self.submodules += weTMRIn
-        self.comb += [req_copy.we.eq(weTMRIn.control)]
         addrTMRIn = TMRInput(TMRreq.addr)
         self.submodules += addrTMRIn
-        self.comb += [req_copy.addr.eq(addrTMRIn.control)]
         validTMRIn = TMRInput(TMRreq.valid)
         self.submodules += validTMRIn
-        self.comb += [req_copy.valid.eq(validTMRIn.control)]
-        self.comb += [req_copy.ready.eq(req.ready)]
 
         # Command buffer ---------------------------------------------------------------------------
         cmd_buffer_layout    = [("we", 1), ("addr", len(req.addr))]
@@ -130,7 +125,11 @@ class BankMachine(Module):
         cmd_buffer = stream.Buffer(cmd_buffer_layout) # 1 depth buffer to detect row change
         self.submodules += cmd_buffer_lookahead, cmd_buffer
         self.comb += [
-            req_copy.connect(cmd_buffer_lookahead.sink, keep={"valid", "ready", "we", "addr"}),
+            #req.connect(cmd_buffer_lookahead.sink, keep={"valid", "ready", "we", "addr"}),
+            cmd_buffer_lookahead.sink.valid.eq(req.valid),
+            req.ready.eq(cmd_buffer_lookahead.sink.ready),
+            cmd_buffer_lookahead.sink.we.eq(req.we),
+            cmd_buffer_lookahead.sink.addr.eq(req.addr),
             cmd_buffer_lookahead.source.connect(cmd_buffer.sink),
             cmd_buffer.source.ready.eq(req.wdata_ready | req.rdata_valid),
             req.lock.eq(cmd_buffer_lookahead.source.valid | cmd_buffer.source.valid),
