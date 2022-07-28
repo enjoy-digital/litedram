1cee67874445de87ac1acad3422cb6557247396d
pbrazzle <prb50@pitt.edu>
2022-07-28 10:28:09 -0400

    req_copy test

--- litedram/core/bankmachine.py
+++ litedram/core/bankmachine.py
@@ -134,10 +134,10 @@ class BankMachine(Module):
         self.submodules += cmd_buffer_lookahead, cmd_buffer
         self.comb += [
             #req_copy.connect(cmd_buffer_lookahead.sink, keep={"valid", "ready", "we", "addr"}),
-            cmd_buffer_lookahead.sink.valid.eq(validTMRIn.control),
+            cmd_buffer_lookahead.sink.valid.eq(req_copy.valid),
             req.ready.eq(cmd_buffer_lookahead.sink.ready),
             cmd_buffer_lookahead.sink.we.eq(weTMRIn.control),
-            cmd_buffer_lookahead.sink.addr.eq(Replicate(0,address_width)),
+            cmd_buffer_lookahead.sink.addr.eq(req.addr),
             cmd_buffer_lookahead.source.connect(cmd_buffer.sink),
             cmd_buffer.source.ready.eq(req.wdata_ready | req.rdata_valid),
             req.lock.eq(cmd_buffer_lookahead.source.valid | cmd_buffer.source.valid),
