73754e696fe28c71aee7dffa0e157e0ad370fd38
pbrazzle <prb50@pitt.edu>
2022-08-24 09:26:10 -0400

    Basic replacement TMR DFII

--- litedram/dfii.py
+++ litedram/dfii.py
@@ -80,3 +80,39 @@ class DFIInjector(Module, AutoCSR):
             self.comb += [phase.cke[i].eq(self._control.fields.cke) for phase in inti.phases]
             self.comb += [phase.odt[i].eq(self._control.fields.odt) for phase in inti.phases if hasattr(phase, "odt")]
         self.comb += [phase.reset_n.eq(self._control.fields.reset_n) for phase in inti.phases if hasattr(phase, "reset_n")]
+   
+# TMRDFIInjector -----------------------------------------------------------------------------------
+   
+class TMRDFIInjector(Module, AutoCSR):
+    def __init__(self, addressbits, bankbits, nranks, databits, nphases=1):
+        inti        = dfi.Interface(addressbits, bankbits, nranks, databits, nphases)
+        self.slave  = dfi.Interface(addressbits, bankbits, nranks, databits, nphases)
+        self.TMRslave = TMRRecord(self.slave)
+        self.master = dfi.Interface(addressbits, bankbits, nranks, databits, nphases)
+
+        self._control = CSRStorage(fields=[
+            CSRField("sel",     size=1, values=[
+                ("``0b0``", "Software (CPU) control."),
+                ("``0b1`",  "Hardware control (default)."),
+            ], reset=0b1), # Defaults to HW control.
+            CSRField("cke",     size=1),
+            CSRField("odt",     size=1),
+            CSRField("reset_n", size=1),
+        ])
+
+        for n, phase in enumerate(inti.phases):
+            setattr(self.submodules, "pi" + str(n), PhaseInjector(phase))
+
+        # # #
+        
+        connect_TMR(self, self.TMRslave, self.slave, master=False)
+
+        self.comb += If(self._control.fields.sel,
+                self.slave.connect(self.master)
+            ).Else(
+                inti.connect(self.master)
+            )
+        for i in range(nranks):
+            self.comb += [phase.cke[i].eq(self._control.fields.cke) for phase in inti.phases]
+            self.comb += [phase.odt[i].eq(self._control.fields.odt) for phase in inti.phases if hasattr(phase, "odt")]
+        self.comb += [phase.reset_n.eq(self._control.fields.reset_n) for phase in inti.phases if hasattr(phase, "reset_n")]
