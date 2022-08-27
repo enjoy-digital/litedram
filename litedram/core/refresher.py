d572b10f70fd02cc7325e31b9421b774d174ac46
pbrazzle <prb50@pitt.edu>
2022-08-27 18:44:53 -0400

    Full refresher TMR

--- litedram/core/refresher.py
+++ litedram/core/refresher.py
@@ -248,11 +248,12 @@ class Refresher(Module):
 
         # Refresh Timer ----------------------------------------------------------------------------
         if timer is None:
-            self.timer = timer
             timer = RefreshTimer(settings.timing.tREFI)
             self.submodules.timer = timer
             self.comb += timer.wait.eq(~timer.done)
-
+        else:
+            self.timer = timer
+            
         # Refresh Postponer ------------------------------------------------------------------------
         postponer = RefreshPostponer(postponing)
         self.submodules.postponer = postponer
