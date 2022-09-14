6df800ea7e0c92a04bf44c6c37a048a103100ecb
pbrazzle <prb50@pitt.edu>
2022-09-14 13:56:37 -0400

    New Verilog

--- litedram/core/multiplexer.py
+++ litedram/core/multiplexer.py
@@ -751,13 +751,13 @@ class TMRMultiplexer(Module, AutoCSR):
 
         # tFAW timing (Four Activate Window) -------------------------------------------------------
         self.submodules.tfawcon = tfawcon = tFAWController(settings.timing.tFAW)
-        self.comb += tfawcon.valid.eq(choose_cmd_int.accept() & choose_cmd_int.activate())
+        self.comb += tfawcon.valid.eq(choose_cmd_accept() & choose_cmd_activate())
 
         self.submodules.tfawcon2 = tfawcon2 = tFAWController(settings.timing.tFAW)
-        self.comb += tfawcon2.valid.eq(choose_cmd_int.accept() & choose_cmd_int.activate())
+        self.comb += tfawcon2.valid.eq(choose_cmd_accept() & choose_cmd_activate())
 
         self.submodules.tfawcon3 = tfawcon3 = tFAWController(settings.timing.tFAW)
-        self.comb += tfawcon3.valid.eq(choose_cmd_int.accept() & choose_cmd_int.activate())
+        self.comb += tfawcon3.valid.eq(choose_cmd_accept() & choose_cmd_activate())
 
         tfawSig = Cat(tfawcon.ready, tfawcon2.ready, tfawcon3.ready)
         tfawVote = TMRInput(tfawSig)
@@ -874,7 +874,7 @@ class TMRMultiplexer(Module, AutoCSR):
                 choose_cmd_int.want_activates.eq(ras_allowed),
                 choose_cmd_int2.want_activates.eq(ras_allowed),
                 choose_cmd_int3.want_activates.eq(ras_allowed),
-                choose_cmd_source.ready.eq(~choose_cmd_int.activate() | ras_allowed),
+                choose_cmd_source.ready.eq(~choose_cmd_activate() | ras_allowed),
                 choose_req_int.cmd.ready.eq(cas_allowed)
             ),
             steerer_sel(steerer, access="read"),
@@ -897,7 +897,7 @@ class TMRMultiplexer(Module, AutoCSR):
                 choose_cmd_int.want_activates.eq(ras_allowed),
                 choose_cmd_int2.want_activates.eq(ras_allowed),
                 choose_cmd_int3.want_activates.eq(ras_allowed),
-                choose_cmd_source.ready.eq(~choose_cmd_int.activate() | ras_allowed),
+                choose_cmd_source.ready.eq(~choose_cmd_activate() | ras_allowed),
                 choose_req_int.cmd.ready.eq(cas_allowed)
             ),
             steerer_sel(steerer, access="write"),
