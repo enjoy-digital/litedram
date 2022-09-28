c493d077fb3c20ff1419fbb9fc955e70161811a8
pbrazzle <prb50@pitt.edu>
2022-09-28 12:47:18 -0400

    New Verilog

--- hoth_dram.v
+++ hoth_dram.v
@@ -7281,7 +7281,7 @@ assign litedramcontroller_syncfifo_din = rhs_array_muxed0;
 assign ce = litedramcontroller_syncfifo_writable;
 assign litedramcontroller_syncfifo_we = (litedramcontroller_syncfifo_writable & rhs_array_muxed1);
 assign request = {litedramcontroller_request};
-assign litedramcontroller_ready = grant;
+assign litedramcontroller_ready = (grant == 1'd0);
 
 // synthesis translate_off
 reg dummy_d_37;
