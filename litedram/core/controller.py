07260d4eb1b53b94bdef84d3b91bfbf1c3fcc54a
pbrazzle <prb50@pitt.edu>
2022-09-28 13:15:43 -0400

    New Verilog

--- litedram/core/controller.py
+++ litedram/core/controller.py
@@ -103,6 +103,7 @@ class LiteDRAMController(Module, AutoCSR):
 
         # Bank Machines ----------------------------------------------------------------------------
         bank_machines = []
+        print(nranks*nbanks)
         for n in range(nranks*nbanks):
             bank_machine = TMRBankMachine(n,
                 address_width = interface.address_width,
